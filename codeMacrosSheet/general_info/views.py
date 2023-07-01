c11ba9c2d351f2174fb2479424888490f38e4563
Mel-Rocha <rochamel73@gmail.com>
2023-07-01 17:16:57 -0300

    v1.10, 01/07/23

--- codeMacrosSheet/general_info/views.py
+++ codeMacrosSheet/general_info/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import GeneralInfo
 from .forms import GeneralInfoForm
 from django.contrib.auth.decorators import login_required
+from macros_sheet.models import MacrosSheet
 
 @login_required
 def general_info_create(request):
@@ -17,14 +18,36 @@ def general_info_create(request):
 @login_required
 def general_info_update(request, pk):
     general_info = get_object_or_404(GeneralInfo, pk=pk)
+
     if request.method == 'POST':
         form = GeneralInfoForm(request.POST, instance=general_info)
         if form.is_valid():
-            form.save()
+            general_info = form.save(commit=False)
+            general_info.save()
+
+            # Atualizar o número de MacrosSheets
+            macros_planner = general_info.macros_planner
+            weeks = general_info.weeks
+            current_weeks = macros_planner.macros_sheets.count()
+
+            if weeks > current_weeks:
+                # Criar MacrosSheets adicionais
+                for week in range(current_weeks + 1, weeks + 1):
+                    macros_sheet = MacrosSheet(
+                        macros_planner=macros_planner,
+                        week_number=week,
+                        # Defina os valores de CHO, PTN, FAT e Kcal de acordo com sua lógica
+                    )
+                    macros_sheet.save()
+            elif weeks < current_weeks:
+                # Excluir MacrosSheets adicionais
+                MacrosSheet.objects.filter(macros_planner=macros_planner, week_number__gt=weeks).delete()
+
             return redirect('general_info:general_info_details', pk=general_info.pk)
     else:
         form = GeneralInfoForm(instance=general_info)
     return render(request, 'general_info/general_info_update.html', {'form': form, 'general_info': general_info})
+
 
 @login_required
 def general_info_delete(request, pk):
--- codeMacrosSheet/macros_sheet/urls.py
+++ codeMacrosSheet/macros_sheet/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'macros_sheet'
 
 urlpatterns = [
-    path('macros_sheet/create/', views.macros_sheet_create, name='macros_sheet_create'),
+    path('macros_planner/<int:macros_planner_id>/macros_sheet/create/', views.macros_sheet_create, name='macros_sheet_create'),
     path('macros_sheet/list/', views.macros_sheet_list, name='macros_sheet_list'),
     path('macros_sheet/<int:pk>/update/', views.macros_sheet_update, name='macros_sheet_update'),
     path('macros_sheet/<int:pk>/details/', views.macros_sheet_details, name='macros_sheet_details'),
