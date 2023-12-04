6a886c388be93368f446b7ed6d9a9d1015dc8bf7
Mel-Rocha <rochamel73@gmail.com>
2023-12-03 22:54:42 -0300

    v2.1, 03/12/23

--- Arnold-Plan-baseLine_1/codeMacrosSheet/proportion_gkg/calcs.py
+++
@@ -1,8 +0,0 @@
-class ProportionGKG:
-    def __init__(self, weight, cho, ptn, fat):
-        self.cho_proportion = cho / weight
-        self.ptn_proportion = ptn / weight
-        self.fat_proportion = fat / weight
-        self.cho = self.cho_proportion * weight
-
-
