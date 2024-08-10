58e5da79d75f191ae51fa659d428830887639c64
Mel-Rocha <rochamel73@gmail.com>
2024-08-10 16:17:23 -0300

    parcial

--- apps/macros/calcs/apps.py
+++ apps/macros/calcs/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PtnConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.macros.ptn'
+    name = 'apps.macros.calcs'
