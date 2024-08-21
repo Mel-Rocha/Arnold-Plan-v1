4db4eb6b0774808ef924113b97e1a65007a27f2e
Mel-Rocha <rochamel73@gmail.com>
2024-08-20 21:42:31 -0300

    certinho a rota de criar meal

--- apps/meal/views.py
+++ apps/meal/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import NotFound
 
+from apps.diet.models import Diet
+from apps.macros_planner.models import MacrosPlanner
 from apps.meal.models import Meal
 from apps.meal.serializers import MealSerializer
 
@@ -8,8 +11,34 @@ class MealViewSet(viewsets.ModelViewSet):
     serializer_class = MealSerializer
 
     def get_queryset(self):
-        return Meal.objects.filter(diet=self.kwargs['diet_id'])
+        macros_planner_id = self.kwargs.get('macros_planner_id')
+        diet_id = self.kwargs.get('diet_id')
+
+        try:
+            macros_planner = MacrosPlanner.objects.get(id=macros_planner_id)
+        except MacrosPlanner.DoesNotExist:
+            raise NotFound(f'MacrosPlanner with id {macros_planner_id} does not exist')
+
+        try:
+            diet = Diet.objects.get(id=diet_id, macros_planner=macros_planner)
+        except Diet.DoesNotExist:
+            raise NotFound(f'Diet with id {diet_id} does not exist or does not belong to MacrosPlanner with id {macros_planner_id}')
+
+        return Meal.objects.filter(diet=diet)
 
     def perform_create(self, serializer):
-        serializer.context['diet_id'] = self.kwargs['diet_id']
-        serializer.save()
+        macros_planner_id = self.kwargs.get('macros_planner_id')
+        diet_id = self.kwargs.get('diet_id')
+
+        try:
+            MacrosPlanner.objects.get(id=macros_planner_id)
+        except MacrosPlanner.DoesNotExist:
+            raise NotFound(f'MacrosPlanner with id {macros_planner_id} does not exist')
+
+        try:
+            diet = Diet.objects.get(id=diet_id, macros_planner__id=macros_planner_id)
+        except Diet.DoesNotExist:
+            raise NotFound(f'Diet with id {diet_id} does not exist or does not belong to MacrosPlanner with id {macros_planner_id}')
+
+        serializer.context['diet_id'] = diet_id
+        serializer.save(diet=diet)
