4b271ae5a9edf56310956ebfe9f30fb6fb99a72c
Mel-Rocha <rochamel73@gmail.com>
2024-08-25 18:57:12 -0300

    aparentemente somente leitura para o nutri especifico, e escrita pro atleta

--- apps/daily_records/serializers.py
+++ apps/daily_records/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
 from apps.daily_records.models import DailyRecords
+from apps.user.models import Athlete
 
 
 class DailyRecordsSerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyRecords
         fields = '__all__'
+        extra_kwargs = {
+            'athlete': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        request = self.context['request']
+        # Obtenha o atleta associado ao usuário logado
+        athlete = Athlete.objects.get(user=request.user)
+        validated_data['athlete'] = athlete
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        # Não permitimos alterar o atleta durante a atualização
+        validated_data.pop('athlete', None)
+        return super().update(instance, validated_data)
--- apps/daily_records/views.py
+++ apps/daily_records/views.py
@@ -4,13 +4,30 @@ from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from apps.daily_records.models import DailyRecords
 from apps.daily_records.serializers import DailyRecordsSerializer
 from apps.core.permissions import IsAthleteUser, IsNutritionistUser
+from apps.user.models import Nutritionist
 
 
 class DailyRecordsViewSet(viewsets.ModelViewSet):
-    queryset = DailyRecords.objects.all()
     serializer_class = DailyRecordsSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_nutritionist:
+            try:
+                # Obtenha a instância do Nutritionist associado ao usuário
+                nutritionist = Nutritionist.objects.get(user=user)
+                print(f"Nutritionist instance: {nutritionist}")  # Verificação para debug
+                # Filtra os DailyRecords pelos atletas associados ao nutricionista
+                return DailyRecords.objects.filter(athlete__nutritionist=nutritionist)
+            except Nutritionist.DoesNotExist:
+                return DailyRecords.objects.none()  # Retorna vazio se o nutricionista não existir
+        elif user.is_athlete:
+            # Filtra os DailyRecords apenas para o atleta autenticado
+            return DailyRecords.objects.filter(athlete__user=user)
+        return DailyRecords.objects.none()
 
     def get_permissions(self):
         if self.request.method in SAFE_METHODS:
-            return [IsAuthenticated(), IsNutritionistUser()]
+            return [IsAuthenticated()]
         return [IsAuthenticated(), IsAthleteUser()]
