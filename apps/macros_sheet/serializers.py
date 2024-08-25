fbd2397bc7178eb3ebac66c48593019e505f8db5
Mel-Rocha <rochamel73@gmail.com>
2024-08-25 01:32:30 -0300

    os cálculos estão funcionando

--- apps/macros_sheet/serializers.py
+++ apps/macros_sheet/serializers.py
@@ -5,12 +5,20 @@ from apps.macros_sheet.models import MacrosSheet
 
 
 class MacrosSheetSerializer(serializers.ModelSerializer):
+    kcal = serializers.ReadOnlyField()
+    kcal_level = serializers.ReadOnlyField()
+    cho_level = serializers.ReadOnlyField()
+    ptn_level = serializers.ReadOnlyField()
+    fat_level = serializers.ReadOnlyField()
+    cho_proportion = serializers.ReadOnlyField()
+    ptn_proportion = serializers.ReadOnlyField()
+    fat_proportion = serializers.ReadOnlyField()
+
     class Meta:
         model = MacrosSheet
-        fields = '__all__'
-        extra_kwargs = {
-            'macros_planner': {'read_only': True}  # MacrosPlanner é read-only e será definido automaticamente
-        }
+        fields = ['macros_planner', 'week', 'cho', 'ptn', 'fat', 'kcal', 'kcal_level', 'cho_level', 'ptn_level', 'fat_level', 'cho_proportion', 'ptn_proportion', 'fat_proportion']
+        extra_kwargs = {'macros_planner': {'read_only': True}, 'week': {'required': False}}
+
 
     def create(self, validated_data):
         macros_planner_id = self.context.get('macros_planner_id')
