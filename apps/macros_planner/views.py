5c04643dd3ee03e3148c749b55525b5d5caec48f
Mel-Rocha <rochamel73@gmail.com>
2024-08-25 13:28:43 -0300

    somente o próprio atleta e o próprio usuário conseguem ver os seus macros planners

--- apps/macros_planner/views.py
+++ apps/macros_planner/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, status
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 
@@ -15,6 +16,38 @@ class MacrosPlannerViewSet(viewsets.ModelViewSet):
         if self.request.method in SAFE_METHODS:
             return [IsAuthenticated()]
         return [IsAuthenticated(), IsNutritionistUser()]
+
+    def get_queryset(self):
+        """
+        Filtra a queryset para garantir que o usuário autenticado
+        só veja os MacrosPlanner aos quais ele está relacionado.
+        """
+        user = self.request.user
+
+        if user.is_nutritionist:
+            # Nutricionista vê apenas os MacrosPlanner dos seus atletas
+            return MacrosPlanner.objects.filter(nutritionist__user=user)
+        elif user.is_athlete:
+            # Atleta vê apenas o seu próprio MacrosPlanner
+            return MacrosPlanner.objects.filter(athlete__user=user)
+        else:
+            return MacrosPlanner.objects.none()
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Sobrescreve o método retrieve para garantir que o usuário só consiga
+        acessar MacrosPlanner aos quais ele está relacionado.
+        """
+        instance = self.get_object()
+        user = self.request.user
+
+        if user.is_nutritionist and instance.nutritionist.user != user:
+            raise PermissionDenied("Você não tem permissão para acessar este MacrosPlanner.")
+        elif user.is_athlete and instance.athlete.user != user:
+            raise PermissionDenied("Você não tem permissão para acessar este MacrosPlanner.")
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
 
     def create(self, request, *args, **kwargs):
