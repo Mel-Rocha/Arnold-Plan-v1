59f5d9b93d35fe7ff50000825d711963a01c5238
Mel-Rocha <rochamel73@gmail.com>
2024-08-25 14:19:58 -0300

    passamos o parametro do nutritionist logado automáticamente para criar o macros_planner

--- apps/macros_planner/serializers.py
+++ apps/macros_planner/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from apps.diet.serializers import DietSerializer
 from apps.macros_planner.models import MacrosPlanner
+from apps.user.models import Nutritionist
 
 
 class MacrosPlannerSerializer(serializers.ModelSerializer):
@@ -10,10 +11,24 @@ class MacrosPlannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = MacrosPlanner
         fields = '__all__'
+        extra_kwargs = {
+            'nutritionist': {'read_only': True},
+        }
 
     def create(self, validated_data):
         diets_data = validated_data.pop('diets', [])
-        macros_planner = MacrosPlanner.objects.create(**validated_data)
+
+        # Obtenha o user logado
+        user = self.context['request'].user
+
+        # Encontre o Nutritionist associado ao user logado
+        try:
+            nutritionist = Nutritionist.objects.get(user=user)
+        except Nutritionist.DoesNotExist:
+            raise serializers.ValidationError({'nutritionist': 'Nutritionist with the current user does not exist.'})
+
+        # Crie o MacrosPlanner e associe o Nutritionist encontrado
+        macros_planner = MacrosPlanner.objects.create(nutritionist=nutritionist, **validated_data)
 
         # Atualize o contexto com o novo ID do MacrosPlanner
         context = self.context
@@ -21,6 +36,9 @@ class MacrosPlannerSerializer(serializers.ModelSerializer):
 
         # Crie as dietas associadas
         for diet_data in diets_data:
+            # Adicione o ID do MacrosPlanner ao contexto da dieta
+            diet_data['macros_planner'] = macros_planner.id
+
             # Crie uma instância do DietSerializer com o contexto correto
             diet_serializer = DietSerializer(data=diet_data, context=context)
             if diet_serializer.is_valid():
